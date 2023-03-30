68036a26543b5d5e28e3df1bfcb7aa0f9faf0cf0
Jason Huang <jason97521@gmail.com>
2023-03-30 09:37:56 -0400

    login ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./app.css"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 import Post from "./pages/Post"
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Post />
+      <Login />
     </div>
   )
 }
