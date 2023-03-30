2fd53c3746350d73eb3eee71becdeb71acd370f4
Jason Huang <jason97521@gmail.com>
2023-03-30 10:24:33 -0400

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Post from "./pages/Post"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
+  const user= true
   return (
-    <div>
-      <Navbar />
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar user={user}/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}/>
+          <Route path="/post/:id" element={user ? <Post/> : <Navigate to="/login" /> }/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom"
 
-const Navbar = () => {
+const Navbar = ({user}) => {
   return (
     <div className="navbar">
-        <span className="logo">Social App</span>
-        <ul className="list">
-            <li className="listItem">
-                <img src="https://images.pexels.com/photos/9442483/pexels-photo-9442483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="avatar" />
-            </li>
-            <li className="listItem">John Doe</li>
-            <li className="listItem">Logout</li>
-        </ul>
+        <span className="logo">
+          <Link to="/" className="link">Social App</Link>
+        </span>
+        {
+          user ? (
+            <ul className="list">
+              <li className="listItem">
+                  <img src="https://images.pexels.com/photos/9442483/pexels-photo-9442483.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="avatar" />
+              </li>
+              <li className="listItem">John Doe</li>
+              <li className="listItem">Logout</li>
+          </ul>
+          ) : (<Link to="/login" className="link">Login</Link>)
+        }
+        
     </div>
   )
 }
